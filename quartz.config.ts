c3c4f29fa84c0372e4a7122fad9534410d178f15
Parkis Utama <parkisutama@outlook.com>
2024-09-28 19:17:43 +0700

    change theme color for light

--- quartz.config.ts
+++ quartz.config.ts
@@ -29,8 +29,8 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
+          light: "#fffdd0",
+          lightgray: "#ffe994",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
